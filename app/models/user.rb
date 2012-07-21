04623186b0dad3f3f56645ff90eb368a2308bd06
Greg Terrono <gterrono@atlassian.com>
2012-07-21 15:59:42 -0700

    Changed the index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password_hash
+
+  validates :email, :presence => true, :uniqueness => true
+  validates :name, :presence => true
+  validates :password_hash, :presence => true
 end
