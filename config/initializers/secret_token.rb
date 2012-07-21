09dabf950782195ab227f5f9de7c5a3eeed6167c
Greg Terrono <gterrono@atlassian.com>
2012-07-21 14:13:12 -0700

    Made a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orderup::Application.config.secret_token = '145232fc34797e32f27dcdbd2e6c004d615a6a78920f2cbe61901f4d31bc27837f6d68fe34dbcae250ce02a3a49d3eb54f244da5828837abafc011370d5b5069'
