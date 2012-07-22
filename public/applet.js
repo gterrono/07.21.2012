99686dd35fa73e25022c0455c587c5845c4f9f65
Anthony J Bonkoski <ajbonkoski@gmail.com>
2012-07-22 06:12:45 +0000

    added new js files

---
+++ public/applet.js
@@ -0,0 +1,99 @@
+MapApplet = (function(){
+    var loc, map;
+    var curOverlay = undefined;
+
+    function init() {
+	loc = new google.maps.LatLng(37.762861, -122.401078);
+	var mapOptions = {
+            center: loc,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+            //mapTypeId: google.maps.MapTypeId.ROADMAP
+            //mapTypeId: google.maps.MapTypeId.SATELLITE
+            //mapTypeId: google.maps.MapTypeId.TERRAIN
+	};
+
+	map = new google.maps.Map($("#map_canvas")[0], mapOptions);
+    }
+
+    function addLocation(placeData){
+	var loc = new google.maps.LatLng(placeData.lat, placeData.lon);
+	var loc_off = new google.maps.LatLng(placeData.lat+0.0006, placeData.lon);
+	var opts = {
+	    position: loc,
+	    map: map,
+	    title: placeData.name
+	};
+	var overlay = new google.maps.InfoWindow({
+	    content: computeOverlayContent(placeData),
+	    position: loc_off
+	});
+	var marker = new google.maps.Marker(opts);
+	google.maps.event.addListener(marker, 'click', function(){openOverlay(overlay)});
+    }
+
+    function openOverlay(overlay){
+	closeOverlays();
+	overlay.open(map);
+	curOverlay = overlay;
+    }
+
+    function closeOverlays(){
+	if(curOverlay)
+	    curOverlay.close();
+	curOverlay = undefined;
+    }
+
+    function computeOverlayContent(placeData){
+	var s = '<div class="overlay">';
+	s += '<div id="overlay-title">'+placeData.name+'</div>';
+	s += '<div id="overlay-user">User: '+placeData.user+'</div>';
+	s += '<div id="overlay-fee">Delivery Fee: $'+placeData.fee+'</div>';
+	s += '<a style="cursor: pointer" onClick="javascript:order_up('+placeData.id+')">Order Up</a></td>';
+	//s += '<a href="requests/new?id='+placeData.id+'">Order Up</a>';
+	s += '</div>';
+	return s;
+    }
+
+    return {
+	init: init,
+	addLocation: addLocation
+    };
+})();
+
+
+function order_up(id){
+    post_to_url('requests/new', {id: id, user: user_id}, "get");
+}
+
+function post_to_url(path, params, method) {
+    method = method || "post"; // Set method to post by default, if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
+
+window.onload = function(){
+    MapApplet.init()
+    for(var i in data){
+	MapApplet.addLocation(data[i]);
+    }
+}
