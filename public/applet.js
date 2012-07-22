06e009dc363546a2605ecdf644b23c0f45d7b81f
Greg Terrono <gterrono@atlassian.com>
2012-07-22 09:32:32 -0700

    Made the notification closeable

--- public/applet.js
+++ public/applet.js
@@ -281,6 +281,7 @@ function displayNotification() {
 
 window.onload = function(){
     //$("#myModal").modal({backdrop: true});
+    $('.close').click(function(e){$(this).parent().fadeOut(1000);});
     update_time_staying_counters();
     setInterval(update_time_staying_counters, 1000);
     dispatch_checkins_request();
